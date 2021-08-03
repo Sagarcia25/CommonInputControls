36df96b74a93232fb277a4e4fcf60fdc471c7a2b
Salvador <Sagarcia2713@gmail.com>
2021-08-02 19:40:11 -0500

    Initial Commit

---
+++ CommonInputControls/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CommonInputControls
+//
+//  Created by Salvador Garcia on 8/2/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
